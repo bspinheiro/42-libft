3c8f4c9e5a3a4d50b1c577fc7b5e4da26e335ecb
Bruno Pinheiro <bda-silv@student.42.rio>
2022-05-09 14:54:54 -0300

    ft_isdigit.c + test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:09:31 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/05/09 12:41:19 by bda-silv         ###   ########.fr       */
+/*   Updated: 2022/05/09 14:52:42 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,30 @@ int	t_isalpha(void)
 			printf("ERR! (%d): %d-%d \n", t, isalpha(t), ft_isalpha(t));
 		t++;
 	}
-	printf("******** DONE!\n");
+	printf("******** DONE! \n\n");
+	return (0);
+}
+
+int	t_isdigit(void)
+{
+	unsigned int	t;
+
+	t = 0;
+	printf("******** ft_isdigit \n");
+	while (t < 256)
+	{
+		if (isdigit(t) != ft_isdigit(t))
+			printf("ERR! (%d): %d-%d \n", t, isdigit(t), ft_isdigit(t));
+		t++;
+	}
+	printf("******** DONE! \n\n");
 	return (0);
 }
 
 int	main(void)
 {
+	printf("\n");
 	t_isalpha();
+	t_isdigit();
 	return (0);
 }
