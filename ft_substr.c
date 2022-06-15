9cb02b5d2b9abc2c9338e9ef5d7d0076bd2fb7a7
Bruno Pinheiro <bda-silv@student.42.rio>
2022-06-15 16:34:31 -0300

    correction: ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,18 +6,10 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 21:17:27 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/06/14 23:38:45 by bda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/15 16:25:51 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	size_t	j;
-	char	*str;
-
+/*
 	str = (char *)malloc(sizeof(*s) * (len + 1));
 	if (!str)
 		return (NULL);
@@ -33,5 +25,29 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		i++;
 	}
 	str[j] = 0;
+	return (str);
+ */
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (ft_strlen(s) < len)
+		len = ft_strlen(s);
+	str = ft_calloc(len + 1, 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i + start] && i < len)
+	{
+		str[i] = s[i + start];
+		i++;
+	}
 	return (str);
 }
