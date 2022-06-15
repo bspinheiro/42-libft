9e6845772af61711f4db877aaf0a412c64292ba4
bda-silv <bda-silv@student.42.rio>
2022-06-15 00:43:25 -0300

    ft_strjoin.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 10:37:45 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/06/14 21:52:23 by bda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/15 00:13:47 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ char			*ft_strnstr(const char *hay, const char *ndl, size_t n);
 char			*ft_strdup(const char *s);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memset(void *s, int c, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
