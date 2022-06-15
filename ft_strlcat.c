f2bead33883a6726d794cbb2b476941386628db5
Bruno Pinheiro <bda-silv@student.42.rio>
2022-06-15 13:54:14 -0300

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,48 +6,11 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 08:31:45 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/06/15 13:40:11 by bda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/15 13:51:24 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-	i = 0;
-	while (dst[i] && i < dstlen)
-	{
-		i++;
-	}
-	j = 0;
-	while (*src && (i + j + 1) < dstlen)
-	{
-		dst[i + j] = src[j];
-		j++;
-	}
-	if (i < dstlen)
-		dst[i + j] = 0;
-	return (i + ft_strlen((char *)src));
-
-	i = 0;
-	while (*(dst + i) && i < dstlen)
-		i++;
-	j = 0;
-	while (*src && (i + j + 1) < dstlen)
-	{
-		*(dst + i + j) = *(src + j);
-		j++;
-	}
-	if (i < dstlen)
-		*(dst + i + j) = 0;
-	return (i + ft_strlen((char *)src));
-}
-*/
-
-#include "libft.h"
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstlen)
-{
-	size_t	i;
-	size_t	j;
-
+/* Using Index
 	i = 0;
 	while (dst[i] && i < dstlen)
 	{
@@ -61,5 +24,26 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstlen)
 	}
 	if (i < dstlen)
 		dst[i + j] = 0;
-	return (i + ft_strlen((char *)src));
+	return (i + ft_strlen(src));
+*/
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstlen)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (*(dst + i) && i < dstlen)
+		i++;
+	j = 0;
+	while (*(src + j) && (i + j + 1) < dstlen)
+	{
+		*(dst + i + j) = *(src + j);
+		j++;
+	}
+	if (i < dstlen)
+		*(dst + i + j) = 0;
+	return (i + ft_strlen(src));
 }
