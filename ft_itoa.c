52dfd40e341d6f0c975923cbcb48afd8b8fef9e3
Bruno Pinheiro <bda-silv@student.42.rio>
2022-06-17 19:12:05 -0300

    ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,28 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/10 12:21:37 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/06/17 17:43:19 by bda-silv         ###   ########.fr       */
+/*   Created: 2022/06/17 15:42:18 by bda-silv          #+#    #+#             */
+/*   Updated: 2022/06/17 19:09:36 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+static size_t	ft_dc(int n)
+{
+	size_t	digits;
+
+	digits = 0;
+	if (n <= 0)
+		digits++;
+	while (n != 0)
+	{
+		n = n / 10;
+		digits++;
+	}
+	return (digits);
+}
+
+char	*ft_itoa(int n)
 {
 	long int	nb;
+	size_t		l;
+	char		*str;
 
 	nb = n;
+	l = ft_dc(n) - 1;
+	str = ft_calloc(ft_dc(n) + 1, sizeof(char));
+	if (!str)
+		return (NULL);
 	if (nb < 0)
 	{
-		ft_putchar_fd('-', fd);
+		str[0] = '-';
 		nb = -nb;
 	}
-	if (nb >= 10)
-		ft_putnbr_fd(nb / 10, fd);
-	ft_putchar_fd(nb % 10 + '0', fd);
+	if (nb == 0)
+		str[0] = '0';
+	while (nb != 0)
+	{
+		str[l--] = nb % 10 + '0';
+		nb /= 10;
+	}
+	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 10:37:45 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/06/15 12:49:20 by bda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/17 15:46:07 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
 char			**ft_split(char const *s, char c);
+char			*ft_itoa(int n);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memset(void *s, int c, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
