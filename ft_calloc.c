0a40ef35fa327f513d1acf6ddb77c5cbd50e10e4
Bruno Pinheiro <bda-silv@student.42.rio>
2022-06-18 18:00:28 -0300

    ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 09:13:46 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/06/09 10:14:58 by bda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/18 17:44:48 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ void	*ft_calloc(size_t count, size_t size)
 		size = 1;
 	}
 	ptr = malloc (count * size);
-	if (ptr == 0)
-		return (0);
+	if (!ptr)
+		return (NULL);
 	else
 		ft_bzero(ptr, count * size);
 	return (ptr);
